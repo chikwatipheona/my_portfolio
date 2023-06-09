898e576d73737c09ec7893db317243453600539a
chikwatipheona <117470572+chikwatipheona@users.noreply.github.com>
2023-06-09 11:34:03 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,81 +1,70 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "portfolio";
+function getConnection() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
+    // Create a connection
+    $conn = new mysqli($servername, $username, $password);
 
-$conn = new mysqli($servername, $username, $password);
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
+    // Create the database if it doesn't exist
+    $sql = "CREATE DATABASE IF NOT EXISTS portifolio";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Database 'portifolio' created successfully.<br>";
+    } else {
+        // echo "Error creating database: " . $conn->error . "<br>";
+    }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // Select the database
+    $conn->select_db("portifolio");
+
+    // Create the 'skills' table
+    $sql = "CREATE TABLE IF NOT EXISTS skills (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        skill VARCHAR(255) NOT NULL,
+        description TEXT
+    )";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Table 'skills' created successfully.<br>";
+    } else {
+        // echo "Error creating table: " . $conn->error . "<br>";
+    }
+
+    // Create the 'booking' table
+    $sql = "CREATE TABLE IF NOT EXISTS booking (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        phone VARCHAR(20) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        message TEXT NOT NULL,
+        service VARCHAR(255) NOT NULL
+    )";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Table 'booking' created successfully.<br>";
+    } else {
+        // echo "Error creating table: " . $conn->error . "<br>";
+    }
+
+    // Create the 'projects' table
+    $sql = "CREATE TABLE IF NOT EXISTS projects (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        status VARCHAR(50)
+    )";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Table 'projects' created successfully.<br>";
+    } else {
+        // echo "Error creating table: " . $conn->error . "<br>";
+    }
+
+    return $conn;
 }
 
 
-$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
-if ($conn->query($sql) === TRUE) {
-    
-} else {
-    echo "Error creating database: " . $conn->error;
-    $conn->close();
-    exit;
-}
-
-
-$conn->close();
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-$sql = "CREATE TABLE IF NOT EXISTS skills (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    skill_name VARCHAR(50) NOT NULL
-)";
-if ($conn->query($sql) === TRUE) {
-    
-} else {
-    echo "Error creating table 'skills': " . $conn->error;
-    $conn->close();
-    exit;
-}
-
-
-$sql = "CREATE TABLE IF NOT EXISTS projects (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    project_name VARCHAR(100) NOT NULL,
-    description TEXT
-)";
-if ($conn->query($sql) === TRUE) {
-    
-} else {
-    echo "Error creating table 'projects': " . $conn->error;
-    $conn->close();
-    exit;
-}
-
-
-$sql = "CREATE TABLE IF NOT EXISTS contacts (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(20)
-)";
-if ($conn->query($sql) === TRUE) {
-    
-} else {
-    echo "Error creating table 'contacts': " . $conn->error;
-    $conn->close();
-    exit;
-}
-
-
-
-return $conn;
- 
 ?>
